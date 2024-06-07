579adb1eb2e01253ef1c6a20867df73a8de2e7e5
Nabin Hamal <68802971+nabinhamal@users.noreply.github.com>
2024-06-07 13:08:26 +0545

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AdminPanel, App, LoginPage } from "./containers";
+import App from "./containers/App"; // Correct the named import to default import if App is a default export
 import "./index.css";
 import { AuthProvider } from "./utils/auth";
+import Loading from "./components/Loading"; // Ensure you have a Loading component
 
+const LoginPage = lazy(() => import("./containers/LoginPage"));
+const AdminPage = lazy(() => import("./containers/AdminPanel"));
 
 ReactDOM.render(
   <AuthProvider>
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/adminpanel" element={<AdminPanel />} />
-        <Route path="/" element={<App />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/adminpanel" element={<AdminPage />} />
+          <Route path="/" element={<App />} />
+        </Routes>
+      </Suspense>
     </Router>
   </AuthProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
